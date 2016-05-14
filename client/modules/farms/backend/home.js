f5591b0864eed13ce244301f76a2bdd7e664407a
KanexKane <kanexkane@gmail.com>
2016-05-14 14:35:16 +0700

    REMOVE aldeed collection2 and autoform

--- client/modules/farms/backend/home.js
+++ client/modules/farms/backend/home.js
@@ -1,12 +1,1 @@
-// AutoForm.hooks({
-//   updateFarmSettingGeneral: {
-//     onSubmit: function (insertDoc, updateDoc, currentDoc) {
-//       if (customHandler(insertDoc)) {
-//         this.done();
-//       } else {
-//         this.done(new Error("Submission failed"));
-//       }
-//       return false;
-//     }
-//   }
-// });
+// 
