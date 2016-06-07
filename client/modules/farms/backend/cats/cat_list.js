54df8da23bac8b89386e72dfd0c60066a1a90bfa
KanexKane <kanexkane@gmail.com>
2016-06-07 18:53:23 +0700

    paging in cats list

--- client/modules/farms/backend/cats/cat_list.js
+++ client/modules/farms/backend/cats/cat_list.js
@@ -28,6 +28,55 @@ Template.MemberFarmCats.helpers({
     },
     searchKeyword: function() {
         return Router.current().params.query.search;
+    },
+    paging: function() {
+        var perPage = parseInt(Router.current().route.options.perpage);
+        var totalPage = Math.floor(Cats.find().count() / perPage);
+        var page = Router.current().params.query.page;
+        var prevPage = 1;
+        var nextPage = 0;
+        var searchKeyword = Router.current().params.query.search;
+
+        if( page === undefined || page === NaN ) { page = 1; }
+
+        page = parseInt(page);
+
+        // ถ้าเพจตอนนี้มากกว่า 1 เพจก่อนหน้าก็ต้องเป็น 1
+        if( page > 1 ) { prevPage = page - 1; }
+
+        var html = '';
+        if( totalPage > 0 && ( nextPage || prevPage ) ) {
+            html += '<div class="paging">';
+            html += '   <div class="col-xs-12">';
+            html += '       <ul class="paging">';
+
+            // หน้าก่อนหน้า ถ้ามากกว่า 1 ถึงจะแสดงและมีลิงค์
+            if ( page === 1 ) {
+                html += '       <li><a href="#"><<</a></li>';
+            } else {
+                html += '       <li><a href="'+ Router.current().route.path({ searchKeyword: searchKeyword }) +'?page='+ (page - 1) +'"><<</a></li>';
+            }
+            
+            for (var i = 1; i <= totalPage; i++) {
+                if ( i === page ) {
+                    html += '   <li><a href="#" class="active">'+ i +'</a></li>';
+                } else {
+                    html += '   <li><a href="'+ Router.current().route.path({ searchKeyword: searchKeyword }) +'?page='+ i +'">'+ i +'</a></li>';
+                }
+            }
+
+            if ( page === totalPage ) {
+                html += '       <li><a href="#">>></a></li>';
+            } else {
+                html += '       <li><a href="'+ Router.current().route.path({ searchKeyword: searchKeyword }) +'?page='+ (page + 1) +'">>></a></li>';
+            }
+            
+            html += '       </ul>';
+            html += '   </div>';
+            html += '   <div class="clearfix"></div>';
+            html += '</div>';
+        }
+        return Spacebars.SafeString(html);
     }
 });
 
