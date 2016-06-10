3df376647cdde91f1f9a69bfad31d9b44f60680d
KanexKane <kanexkane@gmail.com>
2016-06-10 12:03:00 +0700

    fixed a litte bugs

--- client/modules/farms/backend/settings/general.js
+++ client/modules/farms/backend/settings/general.js
@@ -1,6 +1,7 @@
 Template.MemberFarmSettingGeneral.events({
     'keyup [name=farm_url]': function(e){
         var farmUrl = document.getElementsByName('farm_url')[0].value;
+        farmUrl = 'http://catland.online/' + farmUrl;
         document.getElementById('exampleUrl').innerHTML = farmUrl;
     }
 });
