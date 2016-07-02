e7150b90cab75f4a5b8f8f6a082e06de1df1e5cf
KanexKane <kanexkane@gmail.com>
2016-07-02 17:09:50 +0700

    หน้าติดต่อเรา

--- client/modules/farms/frontend/contact_farm.js
+++ client/modules/farms/frontend/contact_farm.js
@@ -8,6 +8,17 @@ Template.FarmContactUs.helpers({
 });
 
 Template.FarmContactUs.onRendered( () => {
+    // <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+
+    // $.getScript("//maps.googleapis.com/maps/api/js", function() {
+    //     var data = Template.instance().
+    //     var mapDiv = document.getElementById('map');
+
+    //     var ads, adsbygoogle;
+    //     ads = '<ins class="adsbygoogle" style="display:block;" data-ad-client="ca-pub-2681012140605765" data-ad-slot="9591021875" data-ad-format="auto"></ins>';
+    //     $('#googleAdsHeader').html(ads);
+    //     return (adsbygoogle = window.adsbygoogle || []).push({});
+    // });
     function initMap() {
         var mapDiv = document.getElementById('map');
         var map = new google.maps.Map(mapDiv, {
